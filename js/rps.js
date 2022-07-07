b8c3c646596ad8c9a41c90563a8217f0d789e36a
Alexander Winters <wintersalex@icloud.com>
2022-07-07 12:56:04 -0700

    Add results div and messages with DOM methods

--- js/rps.js
+++ js/rps.js
@@ -1,15 +1,31 @@
+// Globals
+let playerScore = 0;
+let computerScore = 0;
+let gameCount = 0;
+let roundWinner = "";
+
 // Document Elements
 const bodyContainer = document.querySelector("#body-container");
 const resultsContainer = document.querySelector("#results");
-const playerScoreMsg = document.createElement("p");
-const computerScoreMsg = document.createElement("p");
+const scoreMsg = document.createElement("p");
 const gameNum = document.createElement("p");
-const defaultMsg = document.createElement("p");
+const playerScoreTally = document.createElement("p");
+const computerScoreTally = document.createElement("p");
 const finalGameMsg = document.createElement("h2");
 
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // console.log(button.id);
+    let playerSelection = button.id;
+    game(playerSelection);
+  });
+});
+
+// Game Functions
 // Randomly returns either 'Rock', 'Paper, or 'Scissors'
 function computerPlay() {
-  let compPlayArr = ["Rock", "Paper", "Scissors"];
+  let compPlayArr = ["ROCK", "PAPER", "SCISSORS"];
   let play = Math.floor(Math.random() * compPlayArr.length);
   let result = compPlayArr[play];
   // console.log(result);
@@ -23,120 +39,86 @@ function computerPlay() {
     computerSelection
 */
 function playRound(playerSelection, computerSelection) {
-  let playerSelected = playerSelection.toLowerCase();
-  let message, result;
-
-  let gameResultMsg = document.createElement("p");
-  gameResultMsg.classList.add("gameResultMsg");
-
-  switch (playerSelected) {
-    case "rock-button":
-      message =
-        computerSelection === "Rock"
-          ? "Tie!"
-          : computerSelection === "Paper"
-          ? "You Lose! Paper beats Rock"
-          : "You Win! Rock beats Scissors";
-      gameResultMsg.textContent = message;
-
-      if (message != "Tie!") {
-        result = message.substring(0, 8);
-      } else {
-        result = message;
-      }
-      break;
-
-    case "paper-button":
-      message =
-        computerSelection === "Rock"
-          ? "You Win! Paper beats Rock"
-          : computerSelection === "Paper"
-          ? "Tie!"
-          : "You Lose! Paper beats Rock";
-      gameResultMsg.textContent = message;
-
-      if (message != "Tie!") {
-        result = message.substring(0, 8);
-      } else {
-        result = message;
-      }
-      break;
-
-    case "scissors-button":
-      message =
-        computerSelection === "Rock"
-          ? "You Lose! Rock beats Scissors"
-          : computerSelection === "Paper"
-          ? "You Win! Scissors beats Paper"
-          : "Tie!";
-      gameResultMsg.textContent = message;
-
-      if (message != "Tie!") {
-        result = message.substring(0, 8);
-      } else {
-        result = message;
-      }
-      break;
-
-    default:
-      // console.log("Unknown selection");
-      defaultMsg.classList.add("defaultMsg");
-      defaultMsg.textContent = "Unknown Selection";
-      bodyContainer.appendChild(defaultMsg);
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie";
   }
-  bodyContainer.appendChild(gameResultMsg);
-  return result;
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
+  ) {
+    playerScore++;
+    roundWinner = "player";
+  }
+  if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK")
+  ) {
+    computerScore++;
+    roundWinner = "computer";
+  }
+  postScore(roundWinner, playerSelection, computerSelection);
+}
+
+function postScore(winner, playerSelection, computerSelection) {
+  scoreMsg.classList.add("scoreMsg");
+
+  if (winner === "player") {
+    scoreMsg.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} beats ${computerSelection.toLowerCase()}`;
+    return;
+  }
+  if (winner === "computer") {
+    scoreMsg.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} is beaten by ${computerSelection.toLowerCase()}`;
+    return;
+  }
+
+  scoreMsg.textContent = `${capitalizeFirstLetter(
+    playerSelection
+  )} ties with ${computerSelection.toLowerCase()}`;
+
+  resultsContainer.appendChild(scoreMsg);
+}
+
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5;
 }
 
 // Plays 5 rounds, keeping score and reports the results
 // Calls playround() function inside
-function game() {
-  let resultMap = new Map();
-  let playerScore = 0,
-    computerScore = 0,
-    gameCount = 1;
-
+function game(playerSelection) {
+  if (isGameOver()) {
+    return gameOverMsg();
+  }
+  gameCount++;
   gameNum.classList.add("gameNum");
-  playerScoreMsg.classList.add("playerScoreMsg");
-  computerScoreMsg.classList.add("computerScoreMsg");
+  gameNum.textContent = `Game ${gameCount}`;
 
   let computerSelection = computerPlay();
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      // console.log(button.id);
-      let playerSelection = button.id;
-      resultMap.set(gameCount, playRound(playerSelection, computerSelection));
-    });
-  });
+  playRound(playerSelection, computerSelection);
 
-  if (resultMap.get(gameCount) == "You Win!") {
-    playerScore += 1;
-  } else if (resultMap.get(gameCount) == "You Lose") {
-    computerScore += 1;
-  } else {
-    // Tie
-    playerScore += 0;
-    computerScore += 0;
-  }
-  gameNum.textContent = `Game ${gameCount}`;
-  gameCount++;
-
-  playerScoreMsg.textContent = `Player Score: ${playerScore}`;
-  computerScoreMsg.textContent = `Computer Score: ${computerScore}`;
+  playerScoreTally.classList.add("playerScoreTally");
+  playerScoreTally.textContent = `Player Score: ${playerScore}`;
+  computerScoreTally.classList.add("computerScoreTally");
+  computerScoreTally.textContent = `Computer Score: ${computerScore}`;
 
   resultsContainer.appendChild(gameNum);
-  resultsContainer.appendChild(playerScoreMsg);
-  resultsContainer.appendChild(computerScoreMsg);
-
-  const message =
-    playerScore > computerScore ? "Player Wins!" : "Computer Wins!";
-  finalGameMsg.classList.add("finalGameMsg");
-  finalGameMsg.textContent = message;
-
-  resultsContainer.appendChild(finalGameMsg);
+  resultsContainer.appendChild(playerScoreTally);
+  resultsContainer.appendChild(computerScoreTally);
 }
 
-function gameOver() {}
+// Helper Functions
+function gameOverMsg() {
+  playerScore > computerScore
+    ? (finalGameMsg.textContent = "Player Wins!")
+    : (finalGameMsg.textContent = "Computer Wins!");
+  return resultsContainer.appendChild(finalGameMsg);
+}
 
-game();
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
